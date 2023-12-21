6ff6464d284fbbec77753f4e3f04b0ff9f9b1238
STEAM Gia Thiều <109972185+sgtsince2022@users.noreply.github.com>
2023-12-22 01:17:20 +0700

    Add files via upload

---
+++ CHRISTMAS_TREE/main.py
@@ -0,0 +1,68 @@
+# CHRISTMAS TREE DRAWING
+# using Python Turtle
+# 
+# the code snippets and pattern algorithms included in this project
+# is collected, edited and cleaned up by STEAM GIA THIEU.
+# feel free to edit and share your favourite tree to your homie=).
+#
+# References: Python Turtle Documentation: https://docs.python.org/3/library/turtle.html#
+# Our GitHub: https://github.com/sgtsince2022
+# MERRY CHRISTMAS again from SGT!!!
+
+from property import *
+
+""" init(background_color, show_turtle)
+    
+    background_color: color strings (black is default),
+    show_turtle: 1 => show 
+                 0 => hide (faster drawing speed)
+"""
+init('black', 0)
+
+# CONTENT ADJUSTING AREA (try and see =))
+## snowflakes
+snowflakes_amount = 120 
+height = tree_height 
+## tree and ornaments
+tree_size = 14 
+tree_color = LIGHT_GREEN
+light_color1 = 'tomato'
+light_color2 = 'orange'
+## below the tree
+btt_color1 = 'tomato'
+btt_color2 = 'wheat'
+btt_color3 = PINK
+## message
+ms_content = "MERRY CHRISTMAS!"
+ms_color = LIKELY_CYAN
+ms_size = 30
+
+
+
+
+# int main() {
+drawsnow(snowflakes_amount)  
+up(); goto(0, 0); pd()
+drawstar()
+color(tree_color); backward(height * 4.8)
+tree(tree_size, height, tree_color, light_color1, light_color2)
+trunk()
+below_tree(200, 'tomato', 'wheat', PINK)
+message(ms_content, ms_color, ms_size)
+
+# some additional stuffs
+t1 = Turtle()
+t2 = Turtle()
+wn = Screen()
+
+t1.goto(-430.0,-344.0)
+wn.addshape('CHRISTMAS_TREE\\asset\\LOGO.gif')
+t1.shape('CHRISTMAS_TREE\\asset\\LOGO.gif')
+
+t2.goto(420.0,-344.0)
+wn.addshape('CHRISTMAS_TREE\\asset\\QR.gif')
+t2.shape('CHRISTMAS_TREE\\asset\\QR.gif')
+
+t.done()  
+# return 0;
+# } =)
---
+++ CHRISTMAS_TREE/property.py
@@ -0,0 +1,129 @@
+import turtle as t  
+from turtle import *
+import random as r
+import time
+
+tree_height = 100.0 
+
+# Bảng màu - COLORS DEFINITIONS
+## add thêm màu ở đây
+LIKELY_CYAN = '#52ebeb'
+LIGHT_GREEN = '#42f587'
+LIGHT_BLUE = '#42b3f5'
+LIKELY_BROWN = '#c97538'
+BLUE_TO_BROWN_TRANS = '#7fbc63'
+PINK = '#f046df'
+
+def init(bgcolor: str = 'black', show_turtle: bool = 0):  
+    screensize(bg='black')  
+    speed('fastest') 
+    if show_turtle == 1:
+        st() 
+    else:
+        ht()
+
+def drawsnow(amount: int = 120, density: int = 5, maxSize: int = 7):  
+    t.pensize(2) 
+    for i in range(amount): 
+        t.pencolor("white")  
+        t.pu() 
+        t.setx(r.randint(-350, 350))  
+        t.sety(r.randint(-100, 350)) 
+        t.pd()  
+        dens = density
+        snowsize = r.randint(1, maxSize) 
+        for j in range(dens): 
+            t.forward(int(snowsize))
+            t.backward(int(snowsize))
+            t.right(int(360 / dens))
+
+def drawstar():
+    left(90)
+    forward(3 * tree_height)
+    color("orange", "yellow")  
+    begin_fill()
+    left(126)
+
+    for i in range(5):  
+        forward(tree_height / 5)
+        right(144)  
+        forward(tree_height / 5)
+        left(72)  
+    end_fill()
+
+    right(126)
+
+def drawlight(color1: str = 'tomato', color2: str = 'orange'):  
+    if r.randint(0, 40) == 0:  
+        color(color1)  
+        begin_fill()
+        circle(6)  
+        end_fill()
+    elif r.randint(0, 30) == 1:
+        color(color2)  
+        begin_fill()
+        circle(3)  
+        end_fill()
+    else:
+        color(LIGHT_GREEN)  
+
+def tree(d, s, color_tree: str = LIGHT_GREEN, color_light_1: str = 'tomato', color_light_2: str = 'orange'):
+    color(color_tree)  
+    pensize(1)
+    if d <= 0: return
+    forward(s)
+    tree(d - 1, s * .8)
+    right(120)
+    tree(d - 3, s * .5)
+    drawlight(color_light_1, color_light_2)  
+    right(120)
+    tree(d - 3, s * .5)
+    right(120)  
+    backward(s)
+
+def trunk():
+    pensize(2)
+    color(BLUE_TO_BROWN_TRANS)
+    backward(10.0)
+    color(LIKELY_BROWN)
+    backward(25.0)
+    color('black')
+    backward(50.0)
+    color(LIKELY_BROWN)
+    backward(tree_height- 75.0)
+
+def below_tree(amount: int, color1: str, color2: str, color3: str):
+    pensize(2)
+    for i in range(amount):  
+        a = 200 - 400 * r.random()
+        b = 10 - 20 * r.random()
+        up()
+        forward(b)
+        left(90)
+        forward(a)
+        down()
+        random_color = r.randint(0, 2) 
+        if random_color == 0:
+            color(color1)
+        elif random_color == 1:
+            color(color2)
+        else:
+            color(color3)
+        circle(2)
+        up()    
+        backward(a)
+        right(90)
+        backward(b)
+
+def message(mainms: str = "MERRY CHRISTMAS!", mscolor: str = LIKELY_CYAN, mssize: int = 30):
+    style1 = ("Comic Sans MS", mssize, "bold")
+    forward(25)
+    t.color(mscolor, mscolor) 
+    t.write(mainms, align = "center", font = style1)
+
+    # additional hehe=)
+    style2 = ("Comic Sans MS", 15, "italic")
+    backward(125)
+    t.color(LIGHT_BLUE, LIGHT_BLUE) 
+    t.write("\nfrom\n", align = "center", font = style2)  
+    t.write("STEAM GIA THIEU=)", align = "center", font = style2)  
